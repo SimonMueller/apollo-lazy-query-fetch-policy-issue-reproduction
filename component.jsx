96598291ffabaa1396b9bc31d5b44f947136179b
Simon Müller <SimonMueller@users.noreply.github.com>
2022-07-20 11:03:30 +0200

    initial commit

---
+++ component.jsx
@@ -0,0 +1,20 @@
+import React, {useEffect} from 'react';
+import {gql, useLazyQuery} from "@apollo/client";
+
+export const GET_DATA = gql`
+    query GetData($variable: String) {
+        Object {
+            id
+        }
+    }
+`;
+
+export default function Component({input, fetchPolicy}) {
+    const [getData, { data }] = useLazyQuery(GET_DATA, {fetchPolicy});
+
+    useEffect(() => {
+        getData({variables: {input: "my input"}});
+    }, [input])
+
+    return <div>{JSON.stringify(data)}</div>
+}
---
+++ component.spec.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {render, waitFor} from "@testing-library/react";
+import Component, {GET_DATA} from "./component";
+import {MockedProvider} from "@apollo/client/testing";
+
+it('fetches the data two times as the fetch policy is set to "no-cache"', async () => {
+    const spy = createResultSpy();
+    const view = render(<Component fetchPolicy="no-cache" input="something"/>, {wrapper: wrapper(spy)});
+
+    view.rerender(<Component input={"something2"}/>)
+
+    await waitFor(() => expect(spy).toHaveBeenCalledTimes(2));
+});
+
+it('fetches the data one time as the fetch policy is set to "cache-first"', async () => {
+    const spy = createResultSpy();
+    const view = render(<Component fetchPolicy="no-cache" input="cache-first"/>, {wrapper: wrapper(spy)});
+
+    view.rerender(<Component input={"something2"}/>)
+
+    await waitFor(() => expect(spy).toHaveBeenCalledTimes(1));
+});
+
+function wrapper(resultSpy) {
+    return ({children}) => (<MockedProvider mocks={[createMock(resultSpy)]}>
+        {children}
+    </MockedProvider>)
+}
+
+function createResultSpy() {
+    return jest.fn(() => ({
+        data: {
+            Object: {
+                id: "my id"
+            }
+        }
+    }));
+}
+
+function createMock(resultSpy) {
+    return {
+        request: {
+            query: GET_DATA, variables: {
+                input: "my input"
+            }
+        }, result: resultSpy,
+    };
+}
